d95d4b467ca533732678563720d4f9fcac3046cc
sourface94 <nathan_jf@hotmail.co.uk>
2025-04-01 00:53:53 +0100

    chore: add llama cpp import and basic code

--- curverag/graph.py
+++ curverag/graph.py
@@ -1,7 +1,12 @@
 from typing import List
 
+from llama_cpp import Llama
+
 from curverag.transformations import chunk_text
 from curverag.prompts import entity_relationship_extraction_disparate_prompt
+
+
+
 
 
 class Graph:
@@ -9,7 +14,7 @@ class Graph:
         pass
 
 
-def create_graph(texts: List[str], is_narrative: bool = False):
+def create_graph(texts: List[str], is_narrative: bool = False, llm_model_path="./models/7B/llama-model.gguf", max_tokens=1000):
     """
     Create knowledge graph. 
 
@@ -18,6 +23,22 @@ def create_graph(texts: List[str], is_narrative: bool = False):
     
     # chunk text
     texts = chunk_text(texts)
+
+    # load model
+    llm = Llama(
+      llm_model_path="./models/7B/llama-model.gguf",
+      # n_gpu_layers=-1, # Uncomment to use GPU acceleration
+      # seed=1337, # Uncomment to set a specific seed
+      # n_ctx=2048, # Uncomment to increase the context window
+    )
+
+    for chunk in texts:
+        output = llm(
+            entity_relationship_extraction_disparate_prompt + chunk, # Prompt
+            max_tokens=max_tokens,
+            echo=True # Echo the prompt back in the output
+        ) # Generate a completion, can also call create_completion
+        print(output)
 
 
 
