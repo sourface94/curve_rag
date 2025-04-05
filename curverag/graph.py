4278a153ac1d9ac3a242630fca05d273fa120200
sourface94 <nathan_jf@hotmail.co.uk>
2025-04-06 00:11:55 +0100

    feat: add framework for traverse methods

--- curverag/graph.py
+++ curverag/graph.py
@@ -18,6 +18,7 @@ class Node:
 class Edge:
     node_1_id: str
     node_2_id: str
+    name: str
     direction: bool
     description: str
     attributes: List[str]
@@ -35,10 +36,16 @@ class Graph:
         self.edges.append(edge)
 
     def get_matching_node(self, node: Node):
-        return NotImplementedError
+        for n in self.nodes:
+            if n.id == node.id:
+                return n
+        return
 
     def get_matching_edge(self, edge: Edge):
-        return NotImplementedError
+        for e in self.edges:
+            if e.node_1_id == edge.node_1_id and e.node_2_id == edge.node_2_id and  e.name == edge.name and e.direction == edge.direction:
+                return e
+        return
 
     def upsert(self, sub_graph: Graph):
         if self.is_empty():
@@ -56,6 +63,62 @@ class Graph:
             if matching_edge:
                 self.add_edge(edge)
 
+    def traverse(self, query: str):
+        """Traverse using custom traverse algorithm"""
+        # get each entity in the query using gliner
+        entities = get_entities(query)
+        
+        # look for entities as nodes in the graph
+        entities_in_graph = get_nodes_by_name(entities)
+
+        # get the importance of each entity
+        scores = score_entities(query, entities_in_graph)
+
+        # traverse the graph, giving extra weight to nodes which connect (1 hop, 2 hop, 3 hop etc) two entites together (do this by maintaining a register of visited nodes) and 
+        # weight to edges which have the same name/description as entities not matched as nodes). Traverse for longer for entities with higher scores. Return all nodes that were traversed
+        nodes = self.weighted_traverse(entities, entities_in_graph)
+
+        return nodes
+
+
+    def traverse_personalised_pagerank(self, query: str, top_k: int):
+        """Traverse using personalised pagerank algorithm
+        Use either igraph or networkx implementation
+        """
+        # get each entity in the query using gliner
+        entities = get_entities(query)
+        
+        # look for entities as nodes in the graph
+        entities_in_graph = get_nodes_by_name(entities)
+
+        # get the importance of each node
+        node_ranks = page_rank(...)
+
+        # get the top k nodes
+        return nodes[:top_k]
+
+
+    def traverse_hyperbolic_embeddings(self, query: str, top_k: int):
+        """Traverse using hyperbolic embeddings"""
+        
+        # get each entity in the query using gliner
+        entities = get_entities(query)
+        
+        # look for entities as nodes in the graph
+        entities_in_graph = get_nodes_by_name(entities)
+
+        # get embeddings
+        embeddings = node_embeddings()
+        
+        # get nerest neighburs using embeddings
+        nearest_nodes = nn(embeddings, entities_in_graph)
+        
+        # get nearest neigbour in a 1 and 2 hop fashion when the neighbours join two entites e.g. is a neighboiur of a neighbour
+        nearest_hop_nodes = nn_hop(embeddings, entities_in_graph, hop=2)
+
+
+    # get the top k nodes
+    return nearest_nodes + nearest_hop_nodes
 
 def create_graph(texts: List[str], is_narrative: bool = False, llm_model_path="./models/7B/llama-model.gguf", max_tokens=1000):
     """
@@ -69,7 +132,7 @@ def create_graph(texts: List[str], is_narrative: bool = False, llm_model_path=".
 
     # load model
     llm = Llama(
-      llm_model_path="./models/7B/llama-model.gguf",
+      llm_model_path=llm_model_path,
       # n_gpu_layers=-1, # Uncomment to use GPU acceleration
       # seed=1337, # Uncomment to set a specific seed
       # n_ctx=2048, # Uncomment to increase the context window
@@ -78,6 +141,7 @@ def create_graph(texts: List[str], is_narrative: bool = False, llm_model_path=".
     graph = Graph()
     for chunk in texts:
         sub_graph = llm(
+            # TODO: fix this prompt, use outlines
             entity_relationship_extraction_disparate_prompt + chunk, # Prompt
             max_tokens=max_tokens,
             echo=True # Echo the prompt back in the output
@@ -87,4 +151,8 @@ def create_graph(texts: List[str], is_narrative: bool = False, llm_model_path=".
 
 def learn_embeddings():
     return NotImplementedError
+
+
+
+
 
