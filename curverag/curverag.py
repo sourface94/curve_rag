de669bcd2443a5b857e4ac87b94fbe3479fe0573
sourface94 <nathan_jf@hotmail.co.uk>
2025-05-04 14:12:48 +0100

    feat: main class for curverag

--- curverag/curverag.py
+++ curverag/curverag.py
@@ -2,14 +2,15 @@ from sentence_transformers import SentenceTransformer
 
 from curverag.graph import Graph
 from curverag.prompts import PROMPT
+from curverag.atth.train
 
 
 class RAGQuery:
 
-    def __init__(self, graph: Graph, embedding_model: str):
+    def __init__(self, ll, dataset_name: str, embedding_model: str):
         self.embed_model = SentenceTransformer(embedding_model)
-        self.graph = graph
         self.llm = ''
+        self.dataset_name = dataset_name
 
     def query(self, query: str):
 
@@ -18,6 +19,7 @@ class RAGQuery:
 
         # classify query type
 
+
         # traverse graph
         nodes_and_relationships = self.graph.traverse(query)
         prompt = PROMPT["generate_response_query_with_references"] # use query and nodes_and_relationships
@@ -25,3 +27,19 @@ class RAGQuery:
         prompt = prompt.format(**prompt_args)
         result = self.llm.generate(prompt)
         return result
+
+    def fit(dataset: List[str]):
+        """Training of RAGQuery model
+
+        And Mr RAGQuery said: Thou shalt learn the laws of the vocaublary, learn the words and their relation.
+        """
+
+        # create graph
+        graph = create_graph(self.llm, dataset)
+        dataset = create_graph_dataset(graph, )
+
+        # create embeddings
+        train.train()
+
+
+
