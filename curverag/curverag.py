be85047832b2e4e560ec5fb0788544496ab3ea42
sourface94 <nathan_jf@hotmail.co.uk>
2025-05-13 01:10:23 +0100

    fix: entity lookup

--- curverag/curverag.py
+++ curverag/curverag.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import List, Optional
 
+import numpy as np
 from gliner import GLiNER
 from sentence_transformers import SentenceTransformer
 
@@ -10,10 +11,17 @@ from curverag.graph import create_graph, create_graph_dataset
 
 DEFAULT_ENTITY_TYPES = ["person", "location", "entity", "organisation"]
 DEFAULT_GLINER_MODEL = "urchade/gliner_medium-v2.1"
+DEFAULT_SENTENCE_TRANSFORMER_MODEL = "all-MiniLM-L6-v2"
 
 class CurveRAG:
 
-    def __init__(self, llm, outlines_llm, entity_types: List[str] = DEFAULT_ENTITY_TYPES, gliner_model_name: str = DEFAULT_GLINER_MODEL):
+    def __init__(
+        self,
+        llm, outlines_llm,
+        entity_types: List[str] = DEFAULT_ENTITY_TYPES,
+        gliner_model_name: str = DEFAULT_GLINER_MODEL,
+        sentence_transformer_model_name: str=DEFAULT_SENTENCE_TRANSFORMER_MODEL
+    ):
         self.llm = llm
         self.outlines_llm = outlines_llm
         self.gliner_model = GLiNER.from_pretrained(gliner_model_name)
@@ -21,6 +29,7 @@ class CurveRAG:
         self.graph = None
         self.graph_embedding_model = None
         self.dataset = None
+        self.sentence_model = SentenceTransformer(sentence_transformer_model_name)
 
     @classmethod
     def load_class(
@@ -32,14 +41,17 @@ class CurveRAG:
         graph,
         graph_embedding_model,
         dataset,
+        sentence_transformer_model_name
 
     ):
         inst = cls(
             llm,
             outlines_llm,
             entity_types,
-            gliner_model_name
+            gliner_model_name,
+            sentence_transformer_model_name
         )
+        inst.node_sentence_embeddings = SentenceTransformer(sentence_transformer_model_name).encode([n.name for n in graph.nodes])
         inst.graph = graph
         inst.graph_embedding_model = graph_embedding_model
         inst.dataset = dataset
@@ -59,22 +71,33 @@ class CurveRAG:
         print('creating dataset')
         self.dataset = create_graph_dataset(self.graph, dataset_name)
 
-        # enchance graph with general knowledge that the LLM has - how can we do this?
+        # TODO: enchance graph with general knowledge that the LLM has - how can we do this?
 
         # create embeddings
-        print('dataset type', type(self.dataset))
         print('train kg embeddings')
         self.graph_embedding_model = train(self.dataset)
+        self.node_sentence_embeddings = self.sentence_model.encode([n.name for n in self.graph.nodes])
         
 
-    def query(self, query: str, threshold: float = 0.4, max_tokens: int = 100):
+    def query(self, query: str, additional_entity_types: Optional[List[str]]=None, threshold: float = 0.4, max_tokens: int = 100):
 
         # get query entites
-        entities = self.gliner_model.predict_entities(query, self.entity_types, threshold=threshold)
+        all_entity_types = self.entity_types + additional_entity_types
+        entities = self.gliner_model.predict_entities(query, all_entity_types, threshold=threshold)
         entities = [e['text'] for e in entities]
         print('entities', entities)
         #entities = ['Patient', 'Diabetes Mellitus', 'Metformin', 'Type 2 Diabetes', 'Daily']
 
+        query_embeddings = self.sentence_model.encode(entities + [query])
+        similarities = self.sentence_model.similarity(query_embeddings, self.node_sentence_embeddings)
+        threshold = 0.9
+        similar_indices = [list(np.where(sim_row > threshold)[0]) for sim_row in similarities]
+        similar_indices = list(set([i for s in similar_indices for i in s]))
+        embedding_idx = [similar_indices]
+        nodes_idx_id = {v: k for k, v in self.dataset.nodes_id_idx.items()}
+        print(embedding_idx)
+
+        """
         # get nodes that match entities from graph
         graph_entities = []
         for e in entities:
@@ -84,6 +107,7 @@ class CurveRAG:
         
         # get node embeddings
         embedding_idx = [self.dataset.nodes_id_idx[n.id] for n in graph_entities]
+        """
         entity_node_embs = self.graph_embedding_model.entity.weight.data[embedding_idx]
 
         # get all embeddings
@@ -97,7 +121,7 @@ class CurveRAG:
 
         node_ids
         print('node_ids', node_ids)
-        query_graph = self.graph.get_subgraph(list(set(node_ids+[n.id for n in graph_entities])))
+        query_graph = self.graph.get_subgraph(list(set(node_ids+[nodes_idx_id[id] for id in similar_indices])))
 
         # add descriptions of nodes and entities to prompt, along with query 
         prompt_args = {"query": query, "context": str(query_graph)}
@@ -108,6 +132,5 @@ class CurveRAG:
         result = self.llm(prompt, max_tokens=max_tokens)
 
         return result['choices'][0]['text']
-
 
 
